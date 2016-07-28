aa80ede3eff6205fdca1c0c45da7ab01dc045b73
张羽飞 <myGitHub@yufeiizhang.net>
2016-07-28 14:34:51 -0400

    Create Script_Parameter.m
    Parameter settings

---
+++ Experiment01/Script_Parameter.m
@@ -0,0 +1,20 @@
+%% Script_Parameter
+
+% Number of Agent
+AgentNumber = 1;
+
+% Serial Port of mbed Controller
+SPort1 = 'COM1';
+SPort2 = 'COM2';
+SPort3 = 'COM3';
+SPort4 = 'COM4';
+
+% Frame Pausing
+PauseTime = 0.5;
+
+% Sensor Reading Threshold
+SensorThreshold = 2500;
+
+% Length of Command 
+CommandLength = 501;
+CommandLength1121 = 271;
