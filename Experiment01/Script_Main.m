15c72be52dd3abf07623079f6cb1021839fbf87e
张羽飞 <myGitHub@yufeiizhang.net>
2016-07-28 14:34:12 -0400

    Create Script_Main.m
    Main for MATLAB Experiment files

---
+++ Experiment01/Script_Main.m
@@ -0,0 +1,17 @@
+%% Main
+clc;clear;
+%% Ini
+Script_Parameter;
+display('Script_Parameter Finished');
+Script_Ini;
+display('Script_Ini Finished');
+
+while(1)
+    %% Frame
+    Script_Frame;
+    %% Trigger
+    Script_Trigger;
+end
+
+%% Clean Up
+Script_CleanUp;
